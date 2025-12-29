70f5ed45d05ddfe984916cf5e5319600ac482820
Urb@n <matiasurbaneja@hotmail.com.ar>
2025-12-29 18:47:21 -0300

    logo y user-agent

--- main.go
+++ main.go
@@ -10,19 +10,28 @@ import (
 
 const RUTA = "diccionario_txt/"
 
+var usag_default string = "Chrome/51.0.2704.106 Safari/537.36 OPR/38.0.2220.41 "
 var timeout_f = flag.Int("t", 5, "tiempo de espera de cada solicitud")
 var hilos_f = flag.Int("hl", 500, "concurrencia")
 var url_f = flag.String("url", "", "url del sitio a analizar")
 var dic_f = flag.String("dic", "", "diccionario a utilizar en formato.txt")
+var usrAG = flag.String("usr", usag_default, "user-agent a utilizar")
+
+const LOGO = ` 
+··············
+:GO-WebRaptor:
+··············
+reescrito en Golang`
 
 func main() {
-
 	flag.Parse()
+	fmt.Print(LOGO + "\n\n")
 
 	timeout := *timeout_f
 	hilos := *hilos_f
 	url := *url_f
 	diccionario := *dic_f
+	usr := *usrAG
 
 	limite := make(chan struct{}, hilos)
 	wg := sync.WaitGroup{}
@@ -36,7 +45,7 @@ func main() {
 		go func() {
 			defer func() { <-limite }()
 			defer wg.Done()
-			url_encontrado, codigo := requests.Solicitud(url, linea, timeout)
+			url_encontrado, codigo := requests.Solicitud(url, linea, timeout, usr)
 			if codigo != 0 {
 
 				fmt.Printf("%s status >> %d\n", url_encontrado, codigo)
--- requests/requests.go
+++ requests/requests.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-func Solicitud(url string, linea string, timeout int) (string, int) {
+func Solicitud(url string, linea string, timeout int, usr string) (string, int) {
 	cliente := http.Client{Timeout: time.Second * time.Duration(timeout)}
 
 	urlexp := fmt.Sprintf("%s/%s", url, linea) // url que se prueba
@@ -16,14 +16,14 @@ func Solicitud(url string, linea string, timeout int) (string, int) {
 
 	if reqerr == nil {
 
-		req.Header.Set("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36 Edg/143.0.100.0")
+		req.Header.Set("user-agent", usr)
 		resp, resperr := cliente.Do(req)
 
 		if resperr != nil {
 			return "", 0
 		}
 
-		if !slices.Contains([]int{404, 500}, resp.StatusCode) {
+		if !slices.Contains([]int{404, 500, 502}, resp.StatusCode) {
 
 			return urlexp, resp.StatusCode
 
