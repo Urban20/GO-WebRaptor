88471203248db31b5730bea9bb6f9c895ac3313d
Urb@n <matiasurbaneja@hotmail.com.ar>
2025-12-30 19:36:00 -0300

    agrego busqueda de subdominios

--- requests/requests.go
+++ requests/requests.go
@@ -4,14 +4,24 @@ import (
 	"fmt"
 	v2 "math/rand/v2"
 	"net/http"
+	"regexp"
 	"slices"
+	"strings"
 	"time"
 )
 
-func Solicitud(url string, linea string, timeout int, usr string) (string, int) {
-	cliente := http.Client{Timeout: time.Second * time.Duration(timeout)}
+// crea un url nuevo con el subdominio a probar y la url que se le pasa
+func subdominios(url, linea string) (string, error) {
+	exp, experr := regexp.Compile(`https?\://(\w+)\.`)
+	if experr != nil {
+		return "", experr
+	}
+	sd := exp.FindStringSubmatch(url)[1]
+	return strings.Replace(url, sd, linea, 1), nil
 
-	urlexp := fmt.Sprintf("%s/%s", url, linea) // url que se prueba
+}
+
+func nueva_solicitud(cliente http.Client, urlexp string, usr string) (string, int) {
 
 	req, reqerr := http.NewRequest(http.MethodGet, urlexp, nil)
 	suspendido := v2.Float32()
@@ -32,6 +42,26 @@ func Solicitud(url string, linea string, timeout int, usr string) (string, int) 
 
 		}
 	}
-
 	return "", 0
+}
+
+func Solicitud(url string, linea string, timeout int, usr string, sd bool) (string, int) {
+	cliente := http.Client{Timeout: time.Second * time.Duration(timeout)}
+
+	if sd {
+
+		urlexp, sderr := subdominios(url, linea)
+		if sderr != nil {
+			return "", 0
+		}
+
+		url, codigo := nueva_solicitud(cliente, urlexp, usr)
+		return url, codigo
+
+	}
+
+	urlexp := fmt.Sprintf("%s/%s", url, linea)
+	url, codigo := nueva_solicitud(cliente, urlexp, usr)
+	return url, codigo
+
 }
